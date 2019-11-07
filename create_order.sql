364d227b47f849dd60468d23bff1d9ff76b21a5e
alexrojas431 <53063854+alexrojas431@users.noreply.github.com>
2019-11-06 19:52:36 -0600

    Update create_order.sql

--- create_order.sql
+++ create_order.sql
@@ -1,8 +1,9 @@
-CREATE TABLE order(
-    orderID int auto_increment primary key,
-    partNum int FOREIGN KEY REFERENCES inventory(partNum),
+CREATE TABLE orderHistory(
+    orderID int auto_increment PRIMARY KEY,
+    partNum int,
     partDesc varchar(50),
     price float(8,2),
-    email varchar(50) FOREIGN KEY REFERENCES pInfo(email),
-    addr varchar(100) FOREIGN KEY REFERENCES pInfo(addr),
+    email varchar(50),
+    FOREIGN KEY (partNum) REFERENCES inventory(partNum),
+    FOREIGN KEY (email) REFERENCES pInfo(email),
 );
