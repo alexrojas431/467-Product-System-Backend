8cea4270f6dbc48b580edfd22babe46f38cfdcbc
alexrojas431 <53063854+alexrojas431@users.noreply.github.com>
2019-11-06 20:00:38 -0600

    Delete create_order.sql

--- create_order.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE orderHistory(
-    orderID int auto_increment PRIMARY KEY,
-    partNum int,
-    partDesc varchar(50),
-    price float(8,2),
-    email varchar(50),
-    FOREIGN KEY (partNum) REFERENCES inventory(partNum),
-    FOREIGN KEY (email) REFERENCES pInfo(email),
-);
